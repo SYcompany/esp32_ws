5e59440e046dacff40c500ca16ad8f0eaa4626b3
kwon <ksu619@naver.com>
2024-06-17 00:05:17 +0900

    can_tx, gpio, mcpwm first init

---
+++ can_tx/main/main.c
@@ -0,0 +1,42 @@
+#include "driver/gpio.h"
+#include "driver/twai.h"
+
+void app_main()
+{
+    //Initialize configuration structures using macro initializers
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_21, GPIO_NUM_22, TWAI_MODE_NORMAL);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    //Install TWAI driver
+    if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
+        printf("Driver installed\n");
+    } else {
+        printf("Failed to install driver\n");
+        return;
+    }
+
+    //Start TWAI driver
+    if (twai_start() == ESP_OK) {
+        printf("Driver started\n");
+    } else {
+        printf("Failed to start driver\n");
+        return;
+    }
+
+    //Configure message to transmit
+    twai_message_t message;
+    message.identifier = 0xAAAA;
+    message.extd = 1;
+    message.data_length_code = 4;
+    for (int i = 0; i < 4; i++) {
+        message.data[i] = 0;
+    }
+    
+    //Queue message for transmission
+    if (twai_transmit(&message, pdMS_TO_TICKS(1000)) == ESP_OK) {
+        printf("Message queued for transmission\n");
+    } else {
+        printf("Failed to queue message for transmission\n");
+    }
+}
--- mcpwm/main/main.c
+++ mcpwm/main/main.c
@@ -1,104 +1,80 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_attr.h"
+
 #include "driver/mcpwm.h"
 #include "soc/mcpwm_periph.h"
-#include "esp_intr_alloc.h"
-#include "driver/timer.h"
-#include "freertos/portmacro.h"
+#include "driver/pcnt.h"
 
-#define GPIO_PWM0A_OUT 2   // Motor driver input pin 1
-#define GPIO_PWM0B_OUT 33  // Motor driver input pin 2
-#define TIMER_DIVIDER         16
-#define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)
-#define TIMER_INTERVAL_SEC   (3.0)
 
-volatile float spd = 0;
-volatile bool sw = false;
+#define GPIO_PWM0A_OUT 2    //Set GPIO 15 as PWM0A
+#define GPIO_PWM0B_OUT 3   //Set GPIO 16 as PWM0B
 
-typedef struct {
-    int timer_group;
-    int timer_idx;
-    int alarm_interval;
-    bool auto_reload;
-    portMUX_TYPE timer_mux;  // Critical section management
-} timer_info_t;
+SemaphoreHandle_t g_motor_mux;
+float a = 0;
 
-void IRAM_ATTR timer_group0_isr(void *param)
+static void mcpwm_example_gpio_initialize(void)
 {
-    timer_info_t *info = (timer_info_t *) param;
-    TIMERG0.int_clr_timers.t0 = 1;
-    portENTER_CRITICAL_ISR(&info->timer_mux);
-    
-    
-    if(sw == true)
-    {
-        spd = 100.f;
-        sw = false;
-    }
-    else
-    {
-        spd = 0.f;
-        sw = true;
-        
-    }
-
-    portEXIT_CRITICAL_ISR(&info->timer_mux);
-}
-
-void mcpwm_example_gpio_initialize(void)
-{
+    printf("initializing mcpwm gpio...\n");
     mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, GPIO_PWM0A_OUT);
     mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, GPIO_PWM0B_OUT);
 }
 
-void brushed_motor_forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num, float speed)
+
+void brushed_motor_set_duty(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float speed)
 {
     mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
     mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, speed);
     mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_A, MCPWM_DUTY_MODE_0);
 }
 
+
+
+static void mcpwm_brushed_motor_ctrl_thread(void *arg)
+{
+    while(1)
+    {
+        xSemaphoreTake(g_motor_mux,portTICK_PERIOD_MS);
+        brushed_motor_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0,a);
+        xSemaphoreGive(g_motor_mux);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void mcpwm_brushed_motor_receive_thread(void *arg)
+{
+    while(1)
+    {
+        xSemaphoreTake(g_motor_mux,portTICK_PERIOD_MS);
+        a += 0.01f;
+        if(a > 100)
+        {
+            a = 0;
+        }
+        
+        xSemaphoreGive(g_motor_mux);
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
 void app_main(void)
 {
-    printf("start?");
     mcpwm_example_gpio_initialize();
-
-    mcpwm_config_t pwm_config = {
-        .frequency = 1000,
-        .cmpr_a = 0,
-        .cmpr_b = 0,
-        .counter_mode = MCPWM_UP_COUNTER,
-        .duty_mode = MCPWM_DUTY_MODE_0
-    };
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 1000;    // PWM 주파수: 1000 Hz
+    pwm_config.cmpr_a = 0;          // 초기 듀티 사이클: 0%
+    pwm_config.cmpr_b = 0;          // 초기 듀티 사이클: 0%
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
     mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
 
-    timer_info_t timer_info = {
-        .timer_group = TIMER_GROUP_0,
-        .timer_idx = TIMER_0,
-        .alarm_interval = TIMER_INTERVAL_SEC,
-        .auto_reload = true,
-        .timer_mux = portMUX_INITIALIZER_UNLOCKED  // Initialize the spinlock
-    };
-    timer_config_t config = {
-        .divider = TIMER_DIVIDER,
-        .counter_dir = TIMER_COUNT_UP,
-        .counter_en = TIMER_PAUSE,
-        .alarm_en = TIMER_ALARM_EN,
-        .auto_reload = timer_info.auto_reload,
-    };
-    
-    timer_init(timer_info.timer_group, timer_info.timer_idx, &config);
-    timer_set_counter_value(timer_info.timer_group, timer_info.timer_idx, 0x00000000ULL);
-    timer_set_alarm_value(timer_info.timer_group, timer_info.timer_idx, timer_info.alarm_interval * TIMER_SCALE);
-    timer_enable_intr(timer_info.timer_group, timer_info.timer_idx);
-    timer_isr_register(timer_info.timer_group, timer_info.timer_idx, timer_group0_isr, (void *) &timer_info, ESP_INTR_FLAG_IRAM, NULL);
-
-    timer_start(timer_info.timer_group, timer_info.timer_idx);
-
-    while (1)
+    g_motor_mux = xSemaphoreCreateMutex();
+    while(1)
     {
-        //vTaskDelay(pdMS_TO_TICKS(100));
-        brushed_motor_forward(MCPWM_UNIT_0, MCPWM_TIMER_0, spd);
+        xTaskCreate(mcpwm_brushed_motor_ctrl_thread, "mcpwm_brushed_motor_ctrl_thread", 4096, NULL, 1, NULL);
+        xTaskCreate(mcpwm_brushed_motor_receive_thread, "mcpwm_brushed_motor_recieve_thread", 4096, NULL, 0, NULL);
     }
+    
+    
 }
